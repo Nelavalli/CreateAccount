6fe6fb9c418deff74b4dea146954b38038e70e52
ramakrishna3 <ramakrishna3@7INLPC0G1DEQ.reg1.1bank.dbs.com>
2017-03-05 22:45:24 +0530

    base project

--- src/main/java/com/dbs/capi/CreateAccountController.java
+++ src/main/java/com/dbs/capi/CreateAccountController.java
@@ -1,19 +1,65 @@
 package com.dbs.capi;
 
-import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.spring.boot.FatJarRouter;
-import org.springframework.stereotype.Component;
+import java.util.HashMap;
+import java.util.Map;
 
-@Component
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.restlet.RestletComponent;
+import org.apache.camel.spring.boot.FatJarRouter;
+import org.restlet.Component;
+import org.restlet.ext.spring.SpringServerServlet;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
 public class CreateAccountController extends FatJarRouter  {
 
 	@Override
     public void configure() throws Exception {
+		intercept().to("log:intercepted hello");
+        
+		restConfiguration().component("restlet");
+    	
+        rest("/hello").get().to("direct:hello");
+        
+        from("direct:hello").transform().simple("Hello World!");
         
         from("direct:getPersons")
         	.transform()
-        		.simple("Hello World!");
+        	.simple("Hello World!")
+        		.to("log:bar");
         	
-        
+        /*
+        from("timer:bar")
+        	.transform()
+        	.simple("Hello World! timer")
+        		.to("direct:getPersons");
+        */
     }
+	@Bean
+	public ServletRegistrationBean servletRegistrationBean() {
+	    	
+		SpringServerServlet serverServlet = new SpringServerServlet();
+	    ServletRegistrationBean regBean = new ServletRegistrationBean( serverServlet, "/api/*");
+	    	
+	    Map<String,String> params = new HashMap<String,String>();
+	    	
+	    params.put("org.restlet.component", "restletComponent");
+	    	
+	    regBean.setInitParameters(params);
+	    	
+	    return regBean;
+	}
+	    
+	    
+	    @Bean
+	    public Component restletComponent() {
+	    	return new Component();
+	    }
+	    
+	    @Bean
+	    public RestletComponent restletComponentService() {
+	    	return new RestletComponent(restletComponent());
+	    }
 }
